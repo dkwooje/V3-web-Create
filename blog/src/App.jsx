dffc12c4fe382b3a7fb02a3ea37590e63a16c6b2
ohwooje <dkwooje@naver.com>
2024-12-08 14:42:54 +0900

    기초 3

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -59,6 +59,20 @@ function App() {
             <i className="fa fa-twitter w3-hover-opacity"></i>
             <i className="fa fa-linkedin w3-hover-opacity"></i>
           </div>
+          <footer className="w3-container w3-center w3-padding-48">
+            <a
+              className="w3-margin-bottom"
+              href="https://www.w3schools.com/spaces"
+              title="This website was made with W3schools Spaces. Make your own free website today!"
+              target="_blank"
+            >
+              <img
+                src="https://spaces.w3schools.com/logo_4x.png"
+                width="50"
+                height="50"
+              ></img>
+            </a>
+          </footer>
         </div>
       </div>
     </div>
