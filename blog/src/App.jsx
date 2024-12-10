d6953a81166bccd04962a6c18c4bb8b8fb096604
ohwooje <dkwooje@naver.com>
2024-12-10 13:56:17 +0900

    세번째 html 1: 시작

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,45 +1,7 @@
 import './App.css';
-import { useRef } from 'react';
-import Headline from './menu/Headline';
-import Header from './menu/Header';
-import Trainer from './menu/Trainers';
-import Active from './menu/Active';
-import Contact from './menu/Contact';
-import Footer from './menu/Footer';
-
-export const useScrollTo = () => {
-  const ref = useRef(null);
-
-  const scrollToElement = () => {
-    if (ref.current) {
-      ref.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
-  return [ref, scrollToElement];
-};
 
 function App() {
-  const [contactRef, scrollToContact] = useScrollTo();
-
-  return (
-    <div className="w3-light-white w3-margin">
-      <Headline />
-      <div className="w3-content" style={{ maxWidth: '1600px' }}>
-        <Header />
-        <div className="w3-row-padding" id="Trainers">
-          <Trainer onClick={scrollToContact} />
-        </div>
-        <div className="w3-white" id="Activities">
-          <Active />
-        </div>
-        <div className="w3-white" id="Contact" ref={contactRef}>
-          <Contact />
-        </div>
-        <Footer />
-      </div>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
