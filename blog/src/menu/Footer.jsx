e18dbdb6be8008e4201cc8e758c4fb4fe51461ac
ohwooje <dkwooje@naver.com>
2024-12-10 15:37:03 +0900

    세번째 html : Header 이미지 출력  수정

---
+++ blog/src/menu/Footer.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer className="w3-center w3-light-grey w3-padding-48 w3-large">
+      <p>
+        This website was made with W3schools Spaces. Make your own free website
+        today!
+      </p>
+      <a
+        className="w3-button w3-round-xxlarge w3-dark-grey w3-margin-bottom"
+        href="https://www.w3schools.com/spaces"
+        target="_blank"
+      >
+        Start now
+      </a>
+    </footer>
+  );
+}
--- blog/src/menu/Header.jsx
+++ blog/src/menu/Header.jsx
@@ -1,6 +1,16 @@
 export default function Header() {
   return (
-    <header className="bgimg w3-display-container w3-grayscale-min" id="home">
+    <header
+      className="bgimg w3-display-container w3-grayscale-min"
+      style={{
+        backgroundImage:
+          "url('https://www.w3schools.com/w3images/coffeehouse.jpg')",
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        minHeight: '75vh',
+      }}
+      id="home"
+    >
       <div className="w3-display-bottomleft w3-center w3-padding-large w3-hide-small">
         <span className="w3-tag">Open from 6am to 5pm</span>
       </div>
